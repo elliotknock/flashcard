cd01f2a9721ee95dfc1f96db6c7454707ff0d9d1
Elliot <EKnock211@gmail.com>
2022-06-13 10:38:37 +0100

    Creating new flashcards.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 # ------------------------------------- UI SETUP ------------------------------------- #
 window = Tk()
@@ -10,36 +23,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
+card_title = canvas.create_text(400, 150, font=(FONT_NAME, 40, "italic"))
+card_word = canvas.create_text(400, 263, font=(FONT_NAME, 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, columnspan=2, row=0)
 
 # Buttons
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+next_card()
 
 window.mainloop()
